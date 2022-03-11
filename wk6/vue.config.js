51f689dbe4697b96319abb46e9d3c6387e40fac2
Ying <wgmmfg@gmail.com>
2022-03-11 16:40:57 +0800

    deploy

--- wk6/vue.config.js
+++ wk6/vue.config.js
@@ -3,6 +3,6 @@ const { defineConfig } = require('@vue/cli-service');
 module.exports = defineConfig({
   transpileDependencies: true,
   publicPath: process.env.NODE_ENV === 'production'
-    ? '/vue3-training/wk6/'
+    ? '/vue3-training/wk6/dist/'
     : '/'
 });
